meghasfdc/cilium

commit fa8a499aab9c121c905b7b92e59ef34275fc5a47

endpoint: start a controller to retry regeneration
If a given regeneration fails for an Endpoint, a controller is launched which
will try to regenerate the Endpoint. This ensures that if an Endpoint goes
into `not-ready` state, that it will not stay in that state forever until
some other event which triggers a regeneration occurs.

This is intended to be as least-invasive of a change as possible, as this
will need to be backported to prior releases.

Signed-off by: Ian Vernon <ian@cilium.io>

pkg/endpoint/api.go
@@ -83,7 +83,10 @@ func NewEndpointFromChangeModel(owner regeneration.Owner, base *models.EndpointC
 		hasBPFProgram:    make(chan struct{}, 0),
 		desiredPolicy:    policy.NewEndpointPolicy(owner.GetPolicyRepository()),
 		controllers:      controller.NewManager(),
+		regenFailedChan:  make(chan struct{}, 1),
 	}
+
+	ep.startRegenerationFailureHandler()
 	ep.realizedPolicy = ep.desiredPolicy
 
 	if base.Mac != "" {

pkg/endpoint/endpoint.go
@@ -276,6 +276,8 @@ type Endpoint struct {
 	// plugin which performed the plumbing will enable certain datapath
 	// features according to the mode selected.
 	DatapathConfiguration models.EndpointDatapathConfiguration
+
+	regenFailedChan chan struct{}
 }
 
 // UpdateController updates the controller with the specified name with the
@@ -363,17 +365,20 @@ func (e *Endpoint) waitForProxyCompletions(proxyWaitGroup *completion.WaitGroup)
 // NewEndpointWithState creates a new endpoint useful for testing purposes
 func NewEndpointWithState(owner regeneration.Owner, ID uint16, state string) *Endpoint {
 	ep := &Endpoint{
-		owner:         owner,
-		ID:            ID,
-		OpLabels:      pkgLabels.NewOpLabels(),
-		status:        NewEndpointStatus(),
-		DNSHistory:    fqdn.NewDNSCacheWithLimit(option.Config.ToFQDNsMinTTL, option.Config.ToFQDNsMaxIPsPerHost),
-		state:         state,
-		hasBPFProgram: make(chan struct{}, 0),
-		controllers:   controller.NewManager(),
-		eventQueue:    eventqueue.NewEventQueueBuffered(fmt.Sprintf("endpoint-%d", ID), option.Config.EndpointQueueSize),
-		desiredPolicy: policy.NewEndpointPolicy(owner.GetPolicyRepository()),
+		owner:           owner,
+		ID:              ID,
+		OpLabels:        pkgLabels.NewOpLabels(),
+		status:          NewEndpointStatus(),
+		DNSHistory:      fqdn.NewDNSCacheWithLimit(option.Config.ToFQDNsMinTTL, option.Config.ToFQDNsMaxIPsPerHost),
+		state:           state,
+		hasBPFProgram:   make(chan struct{}, 0),
+		controllers:     controller.NewManager(),
+		eventQueue:      eventqueue.NewEventQueueBuffered(fmt.Sprintf("endpoint-%d", ID), option.Config.EndpointQueueSize),
+		desiredPolicy:   policy.NewEndpointPolicy(owner.GetPolicyRepository()),
+		regenFailedChan: make(chan struct{}, 1),
 	}
+
+	ep.startRegenerationFailureHandler()
 	ep.realizedPolicy = ep.desiredPolicy
 
 	ep.SetDefaultOpts(option.Config.Opts)
@@ -685,6 +690,9 @@ func parseEndpoint(owner regeneration.Owner, strEp string) (*Endpoint, error) {
 	ep.desiredPolicy = policy.NewEndpointPolicy(owner.GetPolicyRepository())
 	ep.realizedPolicy = ep.desiredPolicy
 	ep.controllers = controller.NewManager()
+	ep.regenFailedChan = make(chan struct{}, 1)
+
+	ep.startRegenerationFailureHandler()
 
 	// We need to check for nil in Status, CurrentStatuses and Log, since in
 	// some use cases, status will be not nil and Cilium will eventually

pkg/endpoint/policy.go
@@ -479,8 +479,11 @@ func (e *Endpoint) Regenerate(regenMetadata *regeneration.ExternalRegenerationMe
 		// Free up resources with context.
 		defer cFunc()
 
-		var buildSuccess bool
-		var regenError error
+		var (
+			buildSuccess bool
+			regenError   error
+			canceled     bool
+		)
 
 		select {
 		case result, ok := <-resChan:
@@ -496,14 +499,73 @@ func (e *Endpoint) Regenerate(regenMetadata *regeneration.ExternalRegenerationMe
 				// This may be unnecessary(?) since 'closing' of the results
 				// channel means that event has been cancelled?
 				e.getLogger().Debug("regeneration was cancelled")
+				canceled = true
 			}
 		}
 
+		// If a build is canceled, that means that the Endpoint is being deleted
+		// not that the build failed.
+		if !buildSuccess && !canceled {
+			select {
+			case e.regenFailedChan <- struct{}{}:
+			default:
+				// If we can't write to the channel, that means that it is
+				// full / a regeneration will occur - we don't have to
+				// do anything.
+			}
+		}
 		done <- buildSuccess
 		close(done)
 	}()
 
 	return done
+}
+
+var reasonRegenRetry = "retrying regeneration"
+
+// startRegenerationFailureHandler waits for a build of the Endpoint to fail.
+// Terminates when the given Endpoint is deleted.
+// If a build fails, the controller tries to regenerate the
+// Endpoint until it succeeds. Once the controller succeeds, it will not be
+// ran again unless another build failure occurs. If the call to `Regenerate`
+// fails inside of the controller,
+func (e *Endpoint) startRegenerationFailureHandler() {
+	e.controllers.UpdateController(fmt.Sprintf("endpoint-%s-regeneration-recovery", e.StringID()), controller.ControllerParams{
+		DoFunc: func(ctx context.Context) error {
+			select {
+			case <-e.regenFailedChan:
+				e.getLogger().Debug("received signal that regeneration failed")
+			case <-ctx.Done():
+				e.getLogger().Debug("exiting retrying regeneration goroutine due to endpoint being deleted")
+				return nil
+			}
+
+			if err := e.lockAlive(); err != nil {
+				// We don't need to regenerate because the endpoint is d
+				// disconnecting / is disconnected, exit gracefully.
+				return nil
+			}
+
+			stateTransitionSucceeded := e.setState(StateWaitingToRegenerate, reasonRegenRetry)
+			e.unlock()
+			if !stateTransitionSucceeded {
+				// Another regeneration has already been enqueued.
+				return nil
+			}
+
+			r := &regeneration.ExternalRegenerationMetadata{
+				// TODO (ianvernon) - is there a way we can plumb a parent
+				// context to a controller (e.g., endpoint.aliveCtx)?
+				ParentContext: ctx,
+				Reason:        reasonRegenRetry,
+			}
+			if success := <-e.Regenerate(r); success {
+				return nil
+			}
+			return fmt.Errorf("regeneration recovery failed")
+		},
+		ErrorRetryBaseDuration: 2 * time.Second,
+	})
 }
 
 func (e *Endpoint) notifyEndpointRegeneration(err error) {

commit 873543d6fae00359c5d6240d93d141c40654107b

pkg/endpoint: start RegenerationFailureHandler after assign epID
As an epID is only assigned at the time is exposed, we should only start
the regeneration failure handler after having an ID allocated to the
endpoint. This makes sure the controllers won't override each others
based on the same name (`endpoint-0-regeneration-recovery`).

Fixes: fa8a499aab9c ("endpoint: start a controller to retry regeneration")
Signed-off-by: André Martins <andre@cilium.io>

pkg/endpoint/api.go
@@ -95,7 +95,6 @@ func NewEndpointFromChangeModel(ctx context.Context, owner regeneration.Owner, p
 	ep.aliveCancel = cancel
 	ep.aliveCtx = ctx
 
-	ep.startRegenerationFailureHandler()
 	ep.realizedPolicy = ep.desiredPolicy
 
 	if base.Mac != "" {

pkg/endpoint/endpoint.go
@@ -407,7 +407,6 @@ func NewEndpointWithState(owner regeneration.Owner, proxy EndpointProxy, allocat
 	ctx, cancel := context.WithCancel(context.Background())
 	ep.aliveCancel = cancel
 	ep.aliveCtx = ctx
-	ep.startRegenerationFailureHandler()
 	ep.realizedPolicy = ep.desiredPolicy
 
 	ep.SetDefaultOpts(option.Config.Opts)
@@ -724,8 +723,6 @@ func parseEndpoint(ctx context.Context, owner regeneration.Owner, strEp string) 
 	ctx, cancel := context.WithCancel(ctx)
 	ep.aliveCancel = cancel
 	ep.aliveCtx = ctx
-
-	ep.startRegenerationFailureHandler()
 
 	// We need to check for nil in Status, CurrentStatuses and Log, since in
 	// some use cases, status will be not nil and Cilium will eventually

pkg/endpoint/manager.go
@@ -46,6 +46,7 @@ func (e *Endpoint) Expose(mgr endpointManager) error {
 		logfields.EndpointID: e.ID,
 	})
 
+	e.startRegenerationFailureHandler()
 	// Now that the endpoint has its ID, it can be created with a name based on
 	// its ID, and its eventqueue can be safely started. Ensure that it is only
 	// started once it is exposed to the endpointmanager so that it will be